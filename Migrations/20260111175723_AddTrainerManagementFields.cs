927c98adc644f201c9446ab5b4e4f0bbb8841b55
KaranBastola84 <np05cp4a230154@iic.edu.np>
2026-01-11 23:48:22 +0545

    Feat: Add TrainerRole field to ApplicationUsers for trainer management

---
+++ Migrations/20260111175723_AddTrainerManagementFields.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cms_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrainerManagementFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrainerRole",
+                table: "ApplicationUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrainerRole",
+                table: "ApplicationUsers");
+        }
+    }
+}
