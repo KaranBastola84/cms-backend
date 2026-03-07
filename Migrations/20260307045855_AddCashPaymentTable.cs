f5ddb8626096c61835380875c31e1f1e777f9a4e
KaranBastola84 <np05cp4a230154@iic.edu.np>
2026-03-07 11:15:32 +0545

    feat: Add CashPayments table with necessary fields and relationships

---
+++ Migrations/20260307045855_AddCashPaymentTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace cms_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCashPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CashPayments",
+                columns: table => new
+                {
+                    CashPaymentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Remarks = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    ProcessedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PaidAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CashPayments", x => x.CashPaymentId);
+                    table.ForeignKey(
+                        name: "FK_CashPayments_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CashPayments_PaidAt",
+                table: "CashPayments",
+                column: "PaidAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CashPayments_StudentId",
+                table: "CashPayments",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CashPayments");
+        }
+    }
+}
